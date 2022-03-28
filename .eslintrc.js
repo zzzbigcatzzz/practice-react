e9ce3f9ea4f07e7b79a33f26764e48b075163846
bac.nguyen <bac.nguyen@bluecoglobal.com>
2022-03-28 09:03:25 +0700

    feat: calculator

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard', 'standard-jsx'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-handler-names': 'off',
+    'no-debugger': 'off',
+    'react/prop-types': 0
+
+  }
+}
---
+++ src/components/pages/Calculator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Calculation = () => {
+  return (
+        <div>
+            <h2>CALCULATION</h2>
+        </div>
+  )
+}
